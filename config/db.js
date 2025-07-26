d0d85c011e18b8cf3f72bd8bbe53a871c418a570
Basic-Python <rupeshtha123@gmail.com>
2025-07-26 18:44:08 +0545

    Changing Connection string

--- config/db.js
+++ config/db.js
@@ -3,12 +3,17 @@ const { Pool } = pkg;
 import dotenv from 'dotenv'
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required for Render
 });
+
 
 export default pool;
